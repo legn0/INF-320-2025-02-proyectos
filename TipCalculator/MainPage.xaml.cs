2846e0a58ea14ae17cea4e314839d540bac610b6
legn0 <103703190+legn0@users.noreply.github.com>
2025-05-14 16:22:56 -0400

    base calculadora propina

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Maui.Controls;
+using System.Globalization;
+
+namespace TipCalculator;
+
+public partial class MainPage : ContentPage
+{
+	// int count = 0;
+	private TipCalculatorViewModel viewModel;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		viewModel = new TipCalculatorViewModel();
+		BindingContext = viewModel;
+	}
+
+	private void OnTipPercentageSliderValueChanged(object sender, ValueChangedEventArgs e){
+		viewModel.TipPercentageSliderValueChanged(e.NewValue);
+	}
+
+	
+}
+
+public class IntToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is int intValue)
+        {
+            return intValue.ToString();
+        }
+        return "0";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string stringValue && int.TryParse(stringValue, out int result))
+        {
+            return result;
+        }
+        return 0;
+    }
+}
+
+
+
---
+++ TipCalculator/TipCalculatorViewModel.cs
@@ -0,0 +1,82 @@
+using Android.Health.Connect.DataTypes;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+public partial class TipCalculatorViewModel : ObservableObject
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(PerPersonTotal))]
+    [NotifyPropertyChangedFor(nameof(SubTotal))]
+    [NotifyPropertyChangedFor(nameof(Tip))]
+    private float _bill;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(PerPersonTotal))]
+    [NotifyPropertyChangedFor(nameof(SubTotal))]
+    [NotifyPropertyChangedFor(nameof(Tip))]
+    private int _tipPercentage;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(PerPersonTotal))]
+    [NotifyPropertyChangedFor(nameof(SubTotal))]
+    [NotifyPropertyChangedFor(nameof(Tip))]
+    private int _amountOfPeople;
+
+    [ObservableProperty]
+    private bool _canDecrement;
+
+    //PONERLE OBSERVABLE PROPERTY Y LESERO
+    ///
+    public float PerPersonTotal => (Bill+(Bill*TipPercentage/100))/AmountOfPeople;
+    public float SubTotal => Bill+(Bill*TipPercentage/100);
+    public float Tip => Bill*TipPercentage/100;
+
+    public TipCalculatorViewModel(){ 
+        _bill = 0;
+        _tipPercentage = 10;
+        _amountOfPeople = 1;
+    }
+
+    [RelayCommand]
+    public void OnBillChanged(int value){
+        Bill = value;
+    }
+
+    [RelayCommand]
+    public void OnTenPercentTip(){
+        TipPercentage = 10;
+    }
+
+    [RelayCommand]
+    public void OnFifteenPercentTip(){
+        TipPercentage = 15;
+    }
+
+    [RelayCommand]
+    public void OnTwentyPercentTip(){
+        TipPercentage = 20;
+    }
+
+    [RelayCommand]
+    public void OnTipPercentageSliderValueChanged(int value){
+        TipPercentage = value;
+    }
+
+
+    private void UpdateCanDecrement()
+    {
+        CanDecrement = AmountOfPeople > 1;
+     }
+
+    [RelayCommand]
+    public void OnIncreaseAmountOfPeople(){
+        AmountOfPeople++;
+        UpdateCanDecrement();
+    }
+
+    [RelayCommand]
+    public void OnDecreaseAmountOfPeople(){
+        AmountOfPeople--;
+        UpdateCanDecrement();
+    }
+}
