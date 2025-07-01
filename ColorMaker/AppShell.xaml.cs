a8958a07d62a6aca8000048cb45f9c8f4f9a34cb
MenitoX <pablocontrerasochoa@gmail.com>
2025-07-01 18:01:06 -0400

    borrar lo otro

--- ColorMaker/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ColorMaker;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- ColorMaker/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ColorMaker;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
